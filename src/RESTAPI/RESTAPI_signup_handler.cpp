42133761c08be0933cb3cfb3b69338700c4221fb
stephb9959 <stephane.bourque@gmail.com>
2022-03-01 21:22:57 -0800

    Serialnumber and email validation.

--- src/RESTAPI/RESTAPI_signup_handler.cpp
+++ src/RESTAPI/RESTAPI_signup_handler.cpp
@@ -4,10 +4,25 @@
 
 #include "RESTAPI_signup_handler.h"
 #include "framework/API_Proxy.h"
+#include "framework/MicroService.h"
 
 namespace OpenWifi {
 
     void RESTAPI_signup_handler::DoPost() {
+        //  do dome basic checking before we send this over.
+        auto UserName = GetParameter("email","");
+        Poco::toLowerInPlace(UserName);
+        auto SerialNumber = GetParameter("serialNumber","");
+        Poco::toLowerInPlace(SerialNumber);
+
+        if(!Utils::ValidSerialNumber(SerialNumber)) {
+            return BadRequest(RESTAPI::Errors::InvalidSerialNumber);
+        }
+
+        if(!Utils::ValidEMailAddress(UserName)) {
+            return BadRequest(RESTAPI::Errors::InvalidEmailAddress);
+        }
+
         return API_Proxy(Logger(), Request, Response, uSERVICE_PROVISIONING.c_str(), "/api/v1/signup", 60000);
     }
 
