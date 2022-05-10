cc422214f137c2f83b1e6edeb1c5121b0ed44e5f
stephb9959 <stephane.bourque@gmail.com>
2022-05-09 18:55:38 -0700

    Framework Update.

--- src/RESTAPI/RESTAPI_signup_handler.cpp
+++ src/RESTAPI/RESTAPI_signup_handler.cpp
@@ -10,38 +10,48 @@ namespace OpenWifi {
 
     void RESTAPI_signup_handler::DoPost() {
         //  do dome basic checking before we send this over.
+        std::cout << __LINE__ << std::endl;
         auto UserName = GetParameter("email");
         Poco::toLowerInPlace(UserName);
         Poco::trimInPlace(UserName);
 
+        std::cout << __LINE__ << std::endl;
         auto SerialNumber = GetParameter("macAddress");
         Poco::toLowerInPlace(SerialNumber);
         Poco::trimInPlace(SerialNumber);
+        std::cout << __LINE__ << std::endl;
 
         auto registrationId = GetParameter("registrationId");
         Poco::toLowerInPlace(registrationId);
         Poco::trimInPlace(registrationId);
+        std::cout << __LINE__ << std::endl;
 
         if(!Utils::ValidSerialNumber(SerialNumber)) {
             return BadRequest(RESTAPI::Errors::InvalidSerialNumber);
         }
+        std::cout << __LINE__ << std::endl;
 
         if(!Utils::ValidEMailAddress(UserName)) {
             return BadRequest(RESTAPI::Errors::InvalidEmailAddress);
         }
+        std::cout << __LINE__ << std::endl;
 
         if(registrationId.empty()) {
             return BadRequest(RESTAPI::Errors::InvalidRegistrationOperatorName);
         }
 
+        std::cout << __LINE__ << std::endl;
+
         return API_Proxy(Logger(), Request, Response, ParsedBody_, uSERVICE_PROVISIONING.c_str(), "/api/v1/signup", 60000);
     }
 
     void RESTAPI_signup_handler::DoPut() {
+        std::cout << __LINE__ << std::endl;
         return API_Proxy(Logger(), Request, Response, ParsedBody_, uSERVICE_PROVISIONING.c_str(), "/api/v1/signup", 60000);
     }
 
     void RESTAPI_signup_handler::DoGet() {
+        std::cout << __LINE__ << std::endl;
         return API_Proxy(Logger(), Request, Response, ParsedBody_, uSERVICE_PROVISIONING.c_str(), "/api/v1/signup", 60000);
     }
 
